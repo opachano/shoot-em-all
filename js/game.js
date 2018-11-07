885f301309b73fbd39471d31b7a59761e7245cdd
Oscar Pachano <opachano@gmail.com>
2018-11-06 19:16:52 -0500

    Gets sound file

--- js/game.js
+++ js/game.js
@@ -96,5 +96,24 @@ window.onload = function() {
       keysBeingPressed.splice(theIndex,1)
       }
     }
-  } // if (defeat) {theGame.plane.death(0)}
+  }
 }
+
+// init bunch of sounds
+ion.sound({
+  sounds: [
+      {name: "beer_can_opening"},
+      {name: "bell_ring"},
+      {name: "branch_break"},
+      {name: "button_click"}
+  ],
+
+  // main config
+  path: "static/sounds/",
+  preload: true,
+  multiplay: true,
+  volume: 0.9
+});
+
+// play sound
+ion.sound.play("beer_can_opening");
